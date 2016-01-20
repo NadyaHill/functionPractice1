1601400b640fa06734216fce06dca82810d1e607
Student <student@RU-021.local>
2016-01-19 17:39:51 -0700

    functionPractice1

---
+++ functionPractice1.js
@@ -0,0 +1,57 @@
+// 1
+var tripleFive = function() {
+	for (var i=1; i<4; i++) {
+		console.log("Five!")
+	}
+}
+
+tripleFive();
+
+
+
+
+// 2
+var lastLetter = function () {}
+// NOT DONE YET
+
+
+
+// 3
+var square = function (number) {
+	console.log(number * number)
+}
+square(5);
+
+
+
+// 4
+var negate = function (number) {
+	console.log ("-" + number)
+}
+negate(9)
+
+
+
+// 5
+var toArray = function (a, b, c) {
+	console.log ([a, b, c])
+}
+toArray(1,2,3)
+
+
+
+// 6
+var startsWithA = function (){}
+
+
+
+// 7
+var excite = function (word) {
+	console.log(word + "!!!")
+}
+excite("hello")
+
+
+
+// 8
+var sun = function () {}
